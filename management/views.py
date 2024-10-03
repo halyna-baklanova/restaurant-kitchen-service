9708f8d99bf928d821259b5a9b9c862b96125bd6
Halyna Baklanova <novabalka@gmail.com>
2024-10-03 15:52:27 +0300

    refactor views and add index.html

--- management/views.py
+++ management/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from wheel.cli.convert import convert
+
+from management.models import Dish, Cook
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+    }
+    return render(request, "management/index.html", context=context)
