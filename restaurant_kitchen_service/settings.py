ab121e81819e634a03a2785e0f8f7dc4a99cab85
Halyna Baklanova <novabalka@gmail.com>
2024-10-13 18:45:34 +0300

    settings static file css

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "management.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -120,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = " /static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
