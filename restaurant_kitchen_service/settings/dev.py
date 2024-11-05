406a45413ab21fe79e4e519344584f885e0ea9f1
Halyna <novabalka@gmail.com>
2024-11-05 18:23:13 +0200

    group settings.py

---
+++ restaurant_kitchen_service/settings/dev.py
@@ -0,0 +1,22 @@
+
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
