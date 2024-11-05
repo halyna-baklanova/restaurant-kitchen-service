2c948597b4a7282522af61e27c8707e16284da99
Halyna <novabalka@gmail.com>
2024-11-05 18:30:10 +0200

    add localhost

--- restaurant_kitchen_service/settings/prod.py
+++ restaurant_kitchen_service/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "restaurant-kitchen-service-6pa2.onrender.com"]
 
 
 
