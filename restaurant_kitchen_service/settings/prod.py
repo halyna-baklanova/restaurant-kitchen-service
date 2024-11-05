665e5546fb65c4f087d9c2a3462a910cf3ee7849
Halyna <novabalka@gmail.com>
2024-11-05 18:49:04 +0200

    change localhost

--- restaurant_kitchen_service/settings/prod.py
+++ restaurant_kitchen_service/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "restaurant-kitchen-service-6pa2.onrender.com"]
+ALLOWED_HOSTS = ["restaurant-kitchen-service-6pa2.onrender.com"]
 
 
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
