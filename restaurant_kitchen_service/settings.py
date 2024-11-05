7dfc8326b619eb94ec57244ed710153bf04c939e
Halyna <novabalka@gmail.com>
2024-11-05 10:41:55 +0200

    fix debug in settings.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "8c(e&2uwq_apthw68z#5sdu4zc&#ln&tox%#u-g9%s4e-r5@l*")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-DEBUG = False
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+# DEBUG = False
 
 ALLOWED_HOSTS = []
 
